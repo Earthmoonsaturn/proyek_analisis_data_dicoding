2bd4429328cd3264b22d503b2657a88367253071
Sabrina Tria Amanda <sabrina.ta03@gmail.com>
2024-10-03 12:06:03 +0700

    Update bike_sharing_analyst_dashboard.py

--- bike_sharing_analyst_dashboard.py
+++ bike_sharing_analyst_dashboard.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-day_df = pd.read_csv("bike_sharing_datasets/day.csv")
-hour_df = pd.read_csv("bike_sharing_datasets/hour.csv")
+day_df = pd.read_csv("day.csv")
+hour_df = pd.read_csv("hour.csv")
 
 data1 = hour_df.groupby(['season', "weathersit"])['cnt'].sum().reset_index()
 data2 = hour_df.groupby('mnth')['cnt'].sum().reset_index()
